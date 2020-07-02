fbcd68e7aa2a6117286f0abbe359d62901a7e9c1
Vinícius Scala <viniciusscala@gmail.com>
2020-07-02 01:42:11 -0300

    Feature(componentize):add html and code index.js for the Footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+//css
+import "./Footer.css";
+
+function Footer(){
+    return(
+        <footer>
+            <div>
+                <p>Made with &lt/&gt and &lt3 CITi</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
